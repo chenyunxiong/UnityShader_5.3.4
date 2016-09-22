3ed5a5c92347904d07a37dcc1ae7f1f9a9e1f55a
chenyunxiong <yun8209372@163.com>
2016-09-22 22:05:07 +0800

    firstCreate

---
+++ Assets/Shaders/DiffuseVertexLevel.shader
@@ -0,0 +1,16 @@
+﻿Shader "Custom/DiffuseVertexLevel"{
+	Properties {
+		_Diffuse ("Diffuse", Color) = (1, 1, 1, 1)
+	}
+	SubShader {
+		Pass {
+			Tags { "LightMode" = "ForwardBase" }
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	public Test()
+	{
+		Debug.Log ("test");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start..");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
